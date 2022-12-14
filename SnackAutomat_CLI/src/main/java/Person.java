e96962890f286c101084a847f9984813084f9d07
kaama07 <tsering.anodunkhartsang@edu.tbz.ch>
2022-12-14 12:14:45 +0100

    wrote Person class

--- SnackAutomat_CLI/src/main/java/Person.java
+++ SnackAutomat_CLI/src/main/java/Person.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Person {
+    private String personName;
+    private float budget;
+    private ArrayList<Item> bag;
+
+    public personInfo(String personName, float budget){
+        this.personName = personName;
+        this.budget = budget;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public float getBudget() {
+        return budget;
+    }
+    public void setBudget(float budget) {
+        this.budget = budget;
+    }
 }
