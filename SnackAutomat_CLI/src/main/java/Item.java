35b26b5c15505bd08de65d033f10830c4f23e4b0
kaama07 <tsering.anodunkhartsang@edu.tbz.ch>
2022-12-14 13:26:32 +0100

    item class updated

--- SnackAutomat_CLI/src/main/java/Item.java
+++ SnackAutomat_CLI/src/main/java/Item.java
@@ -4,42 +4,36 @@ public class Item {
     private float price;
     private int amount;
 
+    public Item(String name, int id, float price, int amount) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.amount = amount;
+    }
+
     public String getName() {
         return name;
+    }
+    public int getId() {
+        return id;
+    }
+    public float getPrice() {
+        return price;
+    }
+    public int getAmount() {
+        return amount;
     }
 
     public void setName(String name) {
         this.name = name;
     }
-
-    public int getId() {
-        return id;
-    }
-
     public void setId(int id) {
         this.id = id;
     }
-
-    public float getPrice() {
-        return price;
-    }
-
     public void setPrice(float price) {
         this.price = price;
     }
-
-    public int getAmount() {
-        return amount;
-    }
-
     public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public Item(String name, int id, float price, int amount) {
-        this.name = name;
-        this.id = id;
-        this.price = price;
         this.amount = amount;
     }
 }
