0726eb6d8855d8529a2b2619f7b930cece4b9a8d
Virginia Pelado <virginia.pelado@gmail.com>
2022-12-14 11:08:45 +0100

    Upload Class Item

--- SnackAutomat_CLI/src/main/java/Item.java
+++ SnackAutomat_CLI/src/main/java/Item.java
@@ -1,2 +1,29 @@
 public class Item {
+private String name;
+private int id;
+private float price;
+private int amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public Item(String name, int id, float price, int amount) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.amount = amount;
+    }
 }
